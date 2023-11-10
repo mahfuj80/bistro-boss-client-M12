ac6366dd56d950bdd4e182929b3a51e54c8350cc
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-10 22:52:21 +0600

    Create Parallax and Add Testimonials with react rating

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -4,12 +4,12 @@ import './Featured.css';
 
 const Featured = () => {
   return (
-    <div className="featured-item text-white pt-8 my-20">
+    <div className="featured-item bg-fixed bg-center  text-white pt-8 my-20">
       <SectionTitle
         subHeading={'check it out'}
         heading={'Featured item'}
       ></SectionTitle>
-      <div className="md:flex justify-center items-start pb-20 pt-12 px-36">
+      <div className="md:flex bg-slate-500 opacity-60 justify-center items-start pb-20 pt-12 px-36">
         <div>
           <img src={featuredImg} alt="image" />
         </div>
@@ -24,7 +24,9 @@ const Featured = () => {
             necessitatibus, ut aliquam animi tempore corporis saepe molestias
             optio atque, ipsam explicabo accusamus perspiciatis!
           </p>
-          <button className="btn btn-outline">Order Now</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">
+            Order Now
+          </button>
         </div>
       </div>
     </div>
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,48 @@
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from 'react';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css';
+import { FaQuoteLeft } from 'react-icons/fa';
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch('/reviews.json')
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section className="my-20 md:px-32">
+      <SectionTitle
+        subHeading={'What our client Say'}
+        heading={'Testimonials'}
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews?.map((review) => (
+          <SwiperSlide key={review?._id}>
+            <div className="flex flex-col items-center my-16 mx-24">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review?.rating}
+                readOnly
+              />
+              <p className="text-5xl font-bold mt-4">
+                <FaQuoteLeft></FaQuoteLeft>
+              </p>
+              <p className="py-8 text-center">{review?.details}</p>
+              <h3 className="text-2xl text-orange-400">{review?.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
