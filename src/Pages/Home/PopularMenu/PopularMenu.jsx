2558fd7d65affa1a64c55fd686b11b1dc782872d
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-11 05:17:43 +0600

    Dynamic Page Title By Route Using React Helmet

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -23,6 +23,11 @@ const PopularMenu = () => {
           <MenuItem key={item?._id} item={item}></MenuItem>
         ))}
       </div>
+      <div className="text-center mt-4">
+        <button className="btn btn-outline border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
+      </div>
     </section>
   );
 };
--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,24 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
 const NavBar = () => {
   const navOptions = (
     <>
       <li>
-        <a>Item 1</a>
-      </li>
-      <li tabIndex={0}>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li>
-              <a>Submenu 1</a>
-            </li>
-            <li>
-              <a>Submenu 2</a>
-            </li>
-          </ul>
-        </details>
+        <NavLink to={'/'}>Home</NavLink>
       </li>
       <li>
-        <a>Item 3</a>
+        <NavLink to={'/menu'}>Our Menu</NavLink>
       </li>
     </>
   );
