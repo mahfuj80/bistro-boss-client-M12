f979887168274009565619a7626fb920b0446e67
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-12 03:07:27 +0600

    Display food items on different tabs

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -32,15 +32,15 @@ const Menu = () => {
       {/* Dessert Menu Items*/}
       <MenuCategory
         items={desserts}
-        title={'Dessert'}
+        title={'dessert'}
         img={dessertImg}
       ></MenuCategory>
       {/* Dessert Menu Items*/}
-      <MenuCategory items={pizza} title={'Pizza'} img={pizzaImg}></MenuCategory>
+      <MenuCategory items={pizza} title={'pizza'} img={pizzaImg}></MenuCategory>
       {/* Dessert Menu Items*/}
-      <MenuCategory items={salad} title={'Salad'} img={saladImg}></MenuCategory>
+      <MenuCategory items={salad} title={'salad'} img={saladImg}></MenuCategory>
       {/* Dessert Menu Items*/}
-      <MenuCategory items={soup} title={'Soup'} img={soupImg}></MenuCategory>
+      <MenuCategory items={soup} title={'soup'} img={soupImg}></MenuCategory>
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,16 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Link } from 'react-router-dom';
 import Cover from '../../Shared/Cover/Cover';
 import MenuItem from '../../Shared/MenuItem/MenuItem';
 
-// eslint-disable-next-line react/prop-types
 const MenuCategory = ({ items, title, img }) => {
   return (
     <div className="pt-8">
       {title && <Cover img={img} title={title}></Cover>}
       <div className="grid md:grid-cols-2 gap-4 my-16">
-        {/* eslint-disable-next-line react/prop-types */}
         {items?.map((item) => (
           <MenuItem key={item?._id} item={item}></MenuItem>
         ))}
+      </div>
+
+      <div className="text-center mt-4 pb-4">
+        <Link to={`/order/${title}`}>
+          <button className="btn btn-outline border-0 border-b-4 mt-4">
+            Order Now
+          </button>
+        </Link>
       </div>
     </div>
   );
