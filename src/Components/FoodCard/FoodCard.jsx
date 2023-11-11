e7ac0613c95969d261c89f51761c1d004ff36b5d
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-12 04:48:57 +0600

    Load menu data from server

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -9,11 +9,13 @@ const FoodCard = ({ item }) => {
       <p className="absolute right-0 mr-4 mt-4 px-4 py-1 rounded-lg bg-slate-900 text-white">
         $ {price}
       </p>
-      <div className="card-body text-center">
+      <div className="card-body text-center flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+          <button className="btn btn-outline border-0 bg-slate-100 border-b-4 border-r-orange-400 mt-4">
+            Add To Cart
+          </button>
         </div>
       </div>
     </div>
