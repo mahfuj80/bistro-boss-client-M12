27d1efe7685bbef9d247001562855432eed6efd2
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-16 02:36:06 +0600

    Load all users on the Dashboard page

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,4 +1,5 @@
 import {
+  FaBook,
   FaCalendar,
   FaEnvelope,
   FaHome,
@@ -6,52 +7,95 @@ import {
   FaMagic,
   FaShoppingCart,
   FaStar,
-  FaVoicemail,
+  FaUsers,
+  FaUtensils,
 } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
 import useCart from '../hooks/useCart';
 
 const Dashboard = () => {
   const [cart] = useCart();
+
+  // TODO: get isAdmin value from the database
+  const isAdmin = true;
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4">
-          <li>
-            <NavLink to={'/dashboard/userHome'}>
-              {<FaHome></FaHome>}User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/dashboard/reservation'}>
-              {<FaCalendar></FaCalendar>}Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/dashboard/cart'}>
-              {<FaShoppingCart></FaShoppingCart>}My Cart ({cart?.length})
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/dashboard/review'}>
-              {<FaStar></FaStar>}Add Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to={'/dashboard/bookings'}>
-              {<FaList></FaList>}My Bookings
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to={'/dashboard/adminHome'}>
+                  <FaHome></FaHome>Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/addItems'}>
+                  <FaUtensils></FaUtensils>Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/manageItems'}>
+                  <FaList></FaList>
+                  Manage Item
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/bookings'}>
+                  <FaBook></FaBook>Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/users'}>
+                  <FaUsers></FaUsers>All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to={'/dashboard/userHome'}>
+                  <FaHome></FaHome>User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/reservation'}>
+                  <FaCalendar></FaCalendar>Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/cart'}>
+                  <FaShoppingCart></FaShoppingCart>My Cart ({cart?.length})
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/review'}>
+                  <FaStar></FaStar>Add Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to={'/dashboard/bookings'}>
+                  <FaList></FaList>My Bookings
+                </NavLink>
+              </li>
+            </>
+          )}
           {/* shared nav links */}
           <div className="divider"></div>
           <li>
-            <NavLink to={'/'}>{<FaHome></FaHome>}Home</NavLink>
+            <NavLink to={'/'}>
+              <FaHome></FaHome>Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to={'/order'}>{<FaMagic></FaMagic>}Menu</NavLink>
+            <NavLink to={'/order'}>
+              <FaMagic></FaMagic>Menu
+            </NavLink>
           </li>
           <li>
-            <NavLink to={'/order'}>{<FaEnvelope></FaEnvelope>}Contact</NavLink>
+            <NavLink to={'/order'}>
+              <FaEnvelope></FaEnvelope>Contact
+            </NavLink>
           </li>
         </ul>
       </div>
