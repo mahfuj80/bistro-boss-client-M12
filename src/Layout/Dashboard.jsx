b68543cac09a41b1fd249304e6292079faea85f3
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-16 12:16:09 +0600

    Logout unauthorized access and check is admin

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -12,12 +12,13 @@ import {
 } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
 import useCart from '../hooks/useCart';
+import useAdmin from '../hooks/useAdmin';
 
 const Dashboard = () => {
   const [cart] = useCart();
 
   // TODO: get isAdmin value from the database
-  const isAdmin = true;
+  const [isAdmin] = useAdmin();
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
