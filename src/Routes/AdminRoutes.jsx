93289e8d57afb11ff1ba17527abb5cb90d700534
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-16 12:32:59 +0600

    Make user api secure using verify Admin~

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/prop-types */
-
 import { Navigate, useLocation } from 'react-router-dom';
+import useAdmin from '../hooks/useAdmin';
 import useAuth from '../hooks/useAuth';
 
-const PrivateRoute = ({ children }) => {
+const AdminRoutes = ({ children }) => {
   const { user, loading } = useAuth();
+  const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
-  if (loading) {
+  if (loading || isAdminLoading) {
     return <progress className="progress mx-auto w-56 py-16"></progress>;
   }
-  if (user) {
+  if (user && isAdmin) {
     return children;
   }
   return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>;
 };
 
-export default PrivateRoute;
+export default AdminRoutes;
