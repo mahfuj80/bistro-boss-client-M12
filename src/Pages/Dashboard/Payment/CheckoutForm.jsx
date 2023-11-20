ec45cb17439adb9e79a0ebeec46366852b284ef6
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-20 10:48:30 +0600

    Use Confirm Card Payment to Process Payment

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useEffect, useState } from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import useCart from '../../../hooks/useCart';
+import useAuth from '../../../hooks/useAuth';
 
 const CheckoutForm = () => {
   const [error, setError] = useState('');
   const [clientSecret, setClientSecret] = useState('');
+  const [transactionId, setTransactionId] = useState('');
   const stripe = useStripe();
   const elements = useElements();
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
   const [cart] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item?.price, 0);
 
@@ -44,6 +47,28 @@ const CheckoutForm = () => {
       console.log('payment method', paymentMethod);
       setError('');
     }
+
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || 'anonymous',
+            name: user?.displayName || 'anonymous',
+          },
+        },
+      });
+
+    if (confirmError) {
+      console.log('conirm error', confirmError);
+    } else {
+      console.log('payment intent', paymentIntent);
+      if (paymentIntent?.status === 'succeeded') {
+        console.log('transaction id', paymentIntent?.id);
+        setTransactionId(paymentIntent?.id);
+      }
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -71,6 +96,11 @@ const CheckoutForm = () => {
         Pay
       </button>
       <p className="text-red-600">{error}</p>
+      {transactionId && (
+        <p className="text-green-600 text-xl">
+          Your transaction id: {transactionId}
+        </p>
+      )}
     </form>
   );
 };
