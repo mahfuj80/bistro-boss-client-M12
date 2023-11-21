1880cf8a318af979fd72cb58c085e92b2e346538
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-11-21 12:43:38 +0600

    Load data and Display stats on the admin home

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,12 +1,62 @@
+import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { FaBook, FaDollarSign, FaUserMinus, FaUsers } from 'react-icons/fa';
 const AdminHome = () => {
+  const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
+
+  const { data: stats } = useQuery({
+    queryKey: ['admin-stats'],
+    queryFn: async () => {
+      const res = await axiosSecure.get('/admin-stats');
+      return res?.data;
+    },
+  });
+
   return (
     <div>
       <h2 className="text-3xl">
         <span>Hi, Welcome </span>
         {user?.displayName ? user?.displayName : 'Back'}
       </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign size={35}></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers size={35}></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook size={35}></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUserMinus size={35}></FaUserMinus>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
